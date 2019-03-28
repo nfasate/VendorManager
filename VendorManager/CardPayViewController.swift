eb3b8cbeb56b3adaddece4307f4d712d62959e94
Nilesh_iOS <nfasate@delaplex.in>
2019-03-28 19:30:10 +0530

    initial commit

---
+++ VendorManager/CardPayViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CardPayViewController.swift
+//  VendorManager
+//
+//  Created by NILESH_iOS on 28/03/19.
+//  Copyright © 2019 iDev. All rights reserved.
+//
+
+import UIKit
+
+class CardPayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
